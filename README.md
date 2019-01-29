42d650fa39ab153b72e08f795aec0cc1e4246a5f
oli <olivrg@gmail.com>
2019-01-29 12:56:51 +0000

    finish project

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # socialnews
+
+A social news discovery project to learn how to implement a GraphQL server for a Django application.
+
+## Installation and Usage
+
+Clone the project.
+
+Create a virtual environment:
+
+```bash
+python3.6 -m venv venv
+source venv/bin/activate
+```
+
+Install everything needed:
+
+```bash
+pip install -r requirements.txt
+```
+
+Create the database and run the server:
+
+```
+python socialnews/manage.py migrate
+python socialnews/manage.py runserver
+```
+
+You should be able to access the server on [here](http://localhost:8000/graphql).
+
+## Libraries
+
+django-filter==2.0.0
+django-graphql-jwt==0.1.5
+graphene-django==2.2.0
--- links/schema_relay.py
+++ links/schema_relay.py
@@ -6,31 +6,29 @@ from graphene_django.filter import DjangoFilterConnectionField
 from .models import Link, Vote
 
 
-# 1
 class LinkFilter(django_filters.FilterSet):
+
     class Meta:
         model = Link
         fields = ['url', 'description']
 
 
-# 2
 class LinkNode(DjangoObjectType):
+
     class Meta:
         model = Link
-        # 3
-        interfaces = (graphene.relay.Node, )
+        interfaces = (graphene.relay.Node,)
 
 
 class VoteNode(DjangoObjectType):
+
     class Meta:
         model = Vote
         interfaces = (graphene.relay.Node,)
 
 
 class RelayQuery(graphene.ObjectType):
-    # 4
     relay_link = graphene.relay.Node.Field(LinkNode)
-    # 5
     relay_links = DjangoFilterConnectionField(
         LinkNode, filterset_class=LinkFilter)
 
